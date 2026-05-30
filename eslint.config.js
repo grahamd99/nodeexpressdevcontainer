31d382bcabdf0470cad0045ac00268d068f7a682
grahamd99 <graham.dodsworth@nhs.net>
2026-05-30 20:19:44 +0100

    Add ESLint

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  {
+    ignores: ["node_modules/**"]
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "commonjs",
+      globals: globals.node
+    },
+    rules: {
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "no-console": "off"
+    }
+  }
+];
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const unused = 123;
+
 app.get("/", (req, res) => {
   res.send("Hello from Express running inside a dev container!");
 });
