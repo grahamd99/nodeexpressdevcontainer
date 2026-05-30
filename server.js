f2e56546d9240e8f0ab0f351515cf4c276c56c9a
grahamd99 <graham.dodsworth@nhs.net>
2026-05-30 20:21:38 +0100

    Fix linting issue

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const unused = 123;
-
 app.get("/", (req, res) => {
   res.send("Hello from Express running inside a dev container!");
 });
